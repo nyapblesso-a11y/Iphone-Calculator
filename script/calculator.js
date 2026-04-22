23918c7833301007ea4d02bffb0286ee5de21907
nyapblesso-a11y <nyapblesso@gmail.com>
2026-04-22 11:51:35 +0100

    the appendto diplay function has been implemented

--- script/calculator.js
+++ script/calculator.js
@@ -2,3 +2,8 @@ const display = document.getElementById('display');
 function clearDisplay() {
   display.value = '0';
 }
+
+function appendToDisplay(input) {
+  const lastChar = display.value.slice(-1);
+  const operators = ['+', '-', '*', '/'];
+}
